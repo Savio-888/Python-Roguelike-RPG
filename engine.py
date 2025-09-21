bab78f74bea158f786d7afb97d6942c999339df8
Sávio Pereira Soares <saviopereirasoares6@gmail.com>
2025-09-21 18:36:07 -0300

    updating the engine script

--- engine.py
+++ engine.py
@@ -3,6 +3,7 @@ import sys
 import os
 import glob
 import tcod as englib
+from input_handlers import handle_keys
 
 DATA_FOLDER = "data" # Defining 'data' path.
 FONT_FILE = os.path.join(DATA_FOLDER, "dejavu10x10_gs_tc.png")
@@ -10,14 +11,37 @@ FONT_FILE = os.path.join(DATA_FOLDER, "dejavu10x10_gs_tc.png")
 def main():
     screen_width = 80
     screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+
     englib.console_set_custom_font(FONT_FILE, englib.FONT_TYPE_GRAYSCALE | englib.FONT_LAYOUT_TCOD) # Setting the basic layout of the game.
     englib.console_init_root(screen_width, screen_height, "python roguelike", False) # Sets the size, the title, and if the program will or not be in fullscreen
+    con = englib.console_new(screen_width, screen_height)
+    # Sets keyboard and mouse variables.
+    key = englib.Key()
+    mouse = englib.Mouse()
     while not englib.console_is_window_closed():
         # Sets buffer.
-        englib.console_set_default_foreground(0, englib.white)
-        englib.console_put_char(0, 1, 1, '@', englib.BKGND_NONE)
+        englib.sys_check_for_event(englib.EVENT_KEY_PRESS, key, mouse)
+        englib.console_set_default_foreground(con, englib.white)
+        englib.console_put_char(con, player_x, player_y, '@', englib.BKGND_NONE)
         englib.console_flush()
-        # Sets exit button (ESC).
-        key = englib.console_check_for_keypress()
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get("exit")
+        fullscreen = action.get("fullscreen")
+
+        if move: # making the character move.
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+        if exit :
+            return True
+        if fullscreen:
+            englib.console_set_fullscreen(not englib.console_is_fullscreen())
 if __name__ == "__main__":
     main()
--- input_handlers.py
+++ input_handlers.py
@@ -1,16 +1,18 @@
-import tcod as englib
+# This script handles all input from the game.
+import tcod as libtcod
 def handle_keys(key):
     # Movement Keys
-    if key.vk == englib.KEY_UP:
-        return {"move":(0, -1)}
-    elif key.vk == englib.KEY_DOWN:
-        return {"move":(0, 1)}
-    elif key.vk == englib.KEY_LEFT:
-        return {"move":(-1, 0)}
-    elif key.vk == englib.KEY_RIGHT:
-        return {"move":(1, 0)}
+    if key.vk == libtcod.KEY_UP:
+        return {"move": (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {"move": (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {"move": (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {"move": (1, 0)}
     # Toggle Fullscreen
-    if key.vk == englib.KEY_ENTER and key.lalt:
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
         return {"fullscreen": True}
-    elif key.vk == englib.KEY_ESCAPE:
+    # Exit the Game
+    elif key.vk == libtcod.KEY_ESCAPE:
         return True
